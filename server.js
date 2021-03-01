35c7007ea1f7f633e500ad2f16dd63da9ecf04a1
cahangeorge <cahangeorge@gmail.com>
2021-03-01 15:28:32 +0200

    code for headerparser

--- server.js
+++ server.js
@@ -32,3 +32,8 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+app.get('/api/whoami', (req, res) => {
+  res.json({ ipaddress: req.header('host'), language: req.header('accept-language'), software: req.header('user-agent') });
+});
